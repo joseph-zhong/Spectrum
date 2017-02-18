250b70aa7c604dbb60e5b1c28b1420f1a045e229
John Zeiders <zeiders@usc.edu>
2017-02-18 12:46:10 -0800

    Mockup

--- frontend/src/tooltip.js
+++ frontend/src/tooltip.js
@@ -1,16 +1,21 @@
-import {
-	getValidElements
-} from "./scrape.js"
-
-
 export function createTips() {
 	var tips = $("a[news='true']").map(function() {
         console.log("HMMM")
         var val = "This is link"
         var html = `
-        <p>
-            ${val}
-        </p>
+        <div class="box">
+            <div class="summaries">
+                <h2> Title </h2>
+                <h2> Subtext </h2>
+            </div>
+            <div class="data">
+                <div class="bias">
+                    <h3> Liberal </h3>
+                    <h4> Progress </h4>
+                    <h3> Conservaties </h3>
+                </dib>
+            </div>
+        </div>
         `
         console.log(html)
         Tipped.create($(this), html,{
