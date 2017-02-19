c85cbbfdfc99df0b0b1b4b7f762092306b220676
Jackson Chen <tiger2chen@gmail.com>
2017-02-19 04:05:56 -0800

    change port

--- middleware/scraping/index.js
+++ middleware/scraping/index.js
@@ -3,16 +3,18 @@ var app = express();
 var scraper = require('./scrape.js');
 
 app.get('/', function(req, res) {
+  console.log("GET / Entered");
   res.send('Welcome to Spectrum.');
 })
 
 app.post('/', function(req, res) {
+  console.log("POST / Entered");
   scraper.scrape(function(data) {
     console.log(data);
     res.json(data);
   });
 })
 
-app.listen('3000', function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App listening on port 3000!!");
 })
