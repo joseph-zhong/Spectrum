b333e882a8ea5615684e62c1cc6e27482e094d2f
John Zeiders <zeiders@usc.edu>
2017-02-18 18:33:14 -0800

    Fancy

--- frontend/src/single.js
+++ frontend/src/single.js
@@ -1,19 +1,20 @@
-import {sites} from "./sites.js"
+import {
+	sites
+} from "./sites.js"
 
 const root = "https://jsonplaceholder.typicode.com"
 
 
 export function tag(e) {
 	var link = $(e).attr('href');
-	if (link) { 
+	if (link) {
 		var val = checkValid(link);
 		if (val != 0) {
 			$(e).attr("news", true);
 			$(e).attr("site", val)
+		} else {
+			$(e).attr("news", false)
 		}
-        else {
-            $(e).attr("news", false)
-        }
 	}
 }
 
@@ -24,25 +25,51 @@ export function createTip(e) {
 			url: root + "/posts/1",
 			method: "GET"
 		}).then(function (data) {
+			let bias = 3;
+			let political;
+			let politicHex;
+			switch (bias) {
+				case 1:
+					political = "Liberal";
+					politicHex = "#3751ff";
+					break;
+				case 2:
+					political = "Moderate Liberal";
+					politicHex = "#6fa0ff";
+					break;
+				case 3:
+					political = "Neutral";
+					politicHex = "#b265ff";
+					break;
+				case 4:
+					political = "Moderate Conservative";
+					politicHex = "#ff7070";
+					break;
+				case 5:
+					political = "Conservative";
+					politicHex = "#fe4d4d";
+					break;
+			}
 			let val = data.body;
 			let html = `
-		<div class="box">
-			<div class="political">
-				Liberal
-			</div>
-      <div class="summary">
-          <h3> Summary </h3>
-          <p> ${val} </p>
-      </div>
-			<div class="related">
-				<h3> Related Articles </h3>
-			</div>
-		</div>
-    `
+				<div class="box">
+					<div class="political title" style="background-color: ${politicHex};">
+						${political}
+					</div>
+		      		<div class="summary">
+		        		<div class="title"> Summary </div>
+		       		 		<p> ${val} </p>
+		    	  	</div>
+					<div class="related">
+						<div class="title"> Related Articles </div>
+						<p> Test </p>
+					</div>
+				</div>
+				`
 			Tipped.create($(e), html, {
 				position: "right"
 			});
-            $(e).css("background-color", "red");
+			$(e).css("background-color", "red");
 			console.log("Phat tips");
 		})
 	}
--- frontend/src/tooltip.js
+++ frontend/src/tooltip.js
@@ -2,54 +2,14 @@ var root = "https://jsonplaceholder.typicode.com"
 
 export function createTips() {
 	var tips = $('a[news="true"]').map(function() {
-    let bias = 3;
-		let political;
-		let politicHex;
 
-		switch (bias) {
-			case 1:
-				political = "Liberal";
-				politicHex = "#3751ff";
-				break;
-			case 2:
-				political = "Moderate Liberal";
-				politicHex = "#6fa0ff";
-				break;
-			case 3:
-				political = "Neutral";
-				politicHex = "#b265ff";
-				break;
-			case 4:
-				political = "Moderate Conservative";
-				politicHex = "#ff7070";
-				break;
-			case 5:
-				political = "Conservative";
-				politicHex = "#fe4d4d";
-				break;
-		}
 
 		var self = $(this)
 		$.ajax({
 			url: root + "/posts/1",
 			method: "GET"
 		}).then(function(data) {
-			let val = data.body;
-			let html = `
-				<div class="box">
-					<div class="political title" style="background-color: ${politicHex};">
-						${political}
-					</div>
-		      <div class="summary">
-		        <div class="title"> Summary </div>
-		        <p> ${val} </p>
-		      </div>
-					<div class="related">
-						<div class="title"> Related Articles </div>
-						<p> Test </p>
-					</div>
-				</div>
-		    `
+			
 			Tipped.create($(self), html, {
 				position: "right"
 			});
