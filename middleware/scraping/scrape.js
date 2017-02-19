ef3fe3b26c8e302547cb4995438e7798051c4090
Jackson Chen <tiger2chen@gmail.com>
2017-02-19 02:57:33 -0800

    finish scraping

--- middleware/scraping/scrape.js
+++ middleware/scraping/scrape.js
@@ -1,13 +1,15 @@
 var Nightmare = require('nightmare');
+var cheerio = require('cheerio');
 
-function getLeaning(article) {
-  var cheerio = require('cheerio');
+function getLeaning(article, callback) {
+  var SEARCH_QUERY = "http://www.allsides.com/gnp/tod/index32.php?q=";
+
   var nightmare = Nightmare({ show: true });
 
-  console.log(article);
+  var articleURL = SEARCH_QUERY + encodeURIComponent(article);
 
   nightmare
-    .goto(article)
+    .goto(articleURL)
     .wait('div#draggable > .media > .media-body > .media-headline > a')
     .evaluate(function () {
       return document.body.innerHTML;
@@ -15,39 +17,109 @@ function getLeaning(article) {
     .end()
     .then(function (result) {
       var $ = cheerio.load(result);
-      var biasURL = $('div#draggable > .media > .media-body > .news-source > img').attr('src');
+      var returnObj = {"-2": [], "-1": [], "0": [], "1": [], "2": [], "3": []};
+      $('div.isotope-item').each(function(i, elem) {
+        var tmpArr = [];
+        var re = /(.+)(?:\s-\s)(.+)$/g;
+        var tmpNum = getLeanNumber($(elem).find('.news-source > img').attr('src'));
 
-      switch (biasURL) {
-        case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-left.png"):
-          console.log("Left");
-          return("-2");
+        reRes = re.exec($(elem).find("h6.media-headline").text());
+        tmpArr.push(reRes[1].trim(), reRes[2].trim(), $(elem).find("h6.media-headline > a").attr('href'));
 
-        case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-leaning-left.png"):
-          console.log("Lean Left");
-          return("-1");
+        returnObj[tmpNum].push(tmpArr);
+      })
 
-        case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-center.png"):
-          console.log("Center");
-          return("0");
-
-        case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-leaning-right.png"):
-          console.log("Lean Right");
-          return("1");
-
-        case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-right.png"):
-          console.log("Right");
-          return("2");
-      }
+      var articleNum = getLeanNumber($('div#draggable > .media > .media-body > .news-source > img').attr('src'));
+      callback(articleNum, returnObj);
     })
-    .then(function (pol) {
-      console.log(pol);
-    })
+    // .then(function (pol) {
+    //   console.log(pol);
+    // })
     .catch(function (error) {
       console.error('Search failed:', error);
     });
 }
 
-getLeaning('http://www.allsides.com/gnp/tod/index32.php?q=Friends%20No%20More?%20Jorge%20P%C3%A9rez%20and%20Donald%20Trump')//, function(eval) {
-  // console.log("LOOOL");
-  // console.log(eval);
-// }
+function getLeanNumber(biasURL) {
+  switch (biasURL) {
+    case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-left.png"):
+      return("-2");
+
+    case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-leaning-left.png"):
+      return("-1");
+
+    case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-center.png"):
+      return("0");
+
+    case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-leaning-right.png"):
+      return("1");
+
+    case ("http://www.allsides.com/sites/default/files/styles/bias144x24/public/bias-right.png"):
+      return("2");
+
+    default:
+      return("3");
+  }
+}
+
+function chooseArticle(num, similarArticles) {
+  switch (num) {
+    case "-2":
+      if (similarArticles["0"].length != 0) {
+        return similarArticles["0"].slice(0, 3);
+      }
+      else if (similarArticles["1"].length != 0) {
+        return similarArticles["1"].slice(0, 3);
+      }
+      else if (similarArticles["-1"].length != 0) {
+        return similarArticles["-1"].slice(0, 3);
+      }
+      return similarArticles["2"].slice(0, 3);
+    case "-1":
+      if (similarArticles["1"].length != 0) {
+        return similarArticles["1"].slice(0, 3);
+      }
+      else if (similarArticles["0"].length != 0) {
+          return similarArticles["0"].slice(0, 3);
+      }
+      return similarArticles["2"].slice(0, 3);
+    case "0":
+      var rand = Math.random();
+      if (rand > 0.5 && similarArticles["1"].length != 0) {
+        return similarArticles["1"].slice(0, 3);
+      }
+      else if (similarArticles["-1"].length != 0) {
+        return similarArticles["-1"].slice(0, 3);
+      }
+      else if (rand > 0.5 && similarArticles["2"].length != 0) {
+        return similarArticles["2"].slice(0, 3);
+      }
+      return similarArticles["-2"].slice(0, 3);
+    case "1":
+      if (similarArticles["-1"].length != 0) {
+        return similarArticles["-1"].slice(0, 3);
+      }
+      else if (similarArticles["0"].length != 0) {
+        return similarArticles["0"].slice(0, 3);
+      }
+      return similarArticles["-2"].slice(0, 3);
+    case "2":
+      if (similarArticles["0"].length != 0) {
+        return similarArticles["0"].slice(0, 3);
+      }
+      else if (similarArticles["-1"].length != 0) {
+        return similarArticles["-1"].slice(0, 3);
+      }
+      else if (similarArticles["1"].length != 0) {
+        return similarArticles["1"].slice(0, 3);
+      }
+      return similarArticles["-2"].slice(0, 3);
+    case "3":
+      // Run Joseph's ML code
+      return null;
+  }
+}
+
+getLeaning('Friends No More? Jorge Pérez and Donald Trump', function(num, similarArticles) {
+  console.log(chooseArticle(num, similarArticles));
+})
